f485d5f61e683c278057632ecebb7a7c84338e78
harishkumarreddy3 <145240971+harishkumarreddy3@users.noreply.github.com>
2025-12-15 19:37:00 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+def divide(a: int b: int) -> float:
+    return a / b
+
+
+def main():
+    x = 10
+    y = 
+    print("Add result:", add(x, y))
+    print("Divide result:", divide(x, y))
+
+if __name__ == "__main__":
+    main()
